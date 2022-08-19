e246448fd0b829d4f52e84ef75c45860bdbf854d
Prashant Bansal <109963029+prashantb088@users.noreply.github.com>
2022-08-19 10:50:22 +0530

    Update bankModel.java

--- bankModel.java
+++ bankModel.java
@@ -1,51 +1,53 @@
-package first.bank.model;
+package com.bankapp.model;
 
 public class bankModel {
-    private int accNumber;
-    private int accBalance;
-    private String accHolder;
-    private String accPassword;
+	//public void display() {
+	//	System.out.println("this is model ");
+	//}
+	
+	private int accNumber;
+	private int accBalance;
+	private String accHolderName;
+	private String accpassword;
+	
+	@Override
+	public String toString() {
+		return "bankModel [accNumber=" + accNumber + ", accBalance=" + accBalance + ", accHolderName=" + accHolderName
+				+ ", accpassword=" + accpassword + "]";
+	}
+	public int getAccNumber() {
+		return accNumber;
+	}
+	public void setAccNumber(int accNumber) {
+		this.accNumber = accNumber;
+	}
+	public int getAccBalance() {
+		return accBalance;
+	}
+	public void setAccBalance(int accBalance) {
+		this.accBalance = accBalance;
+	}
+	public String getAccHolderName() {
+		return accHolderName;
+	}
+	public void setAccHolderName(String accHolderName) {
+		this.accHolderName = accHolderName;
+	}
+	public String getAccpassword() {
+		return accpassword;
+	}
+	public void setAccpassword(String accpassword) {
+		this.accpassword = accpassword;
+	}
+	////////////////////////////////////////////
+	public void depositAmount(int amount) {
+		this.accBalance +=amount;
+	}
+	
+	public void withdrawAmount(int amount) {
+		this.accBalance -=amount;
+	}
 
-    public int getAccNumber() {
-        return accNumber;
-    }
-
-    public void setAccNumber(int accNumber) {
-        this.accNumber = accNumber;
-    }
-
-    public int getAccBalance() {
-        return accBalance;
-    }
-
-    public void setAccBalance(int accBalance) {
-        this.accBalance = accBalance;
-    }
-
-    public String getAccHolder() {
-        return accHolder;
-    }
-
-    public void setAccHolder(String accHolder) {
-        this.accHolder = accHolder;
-    }
-
-    public String getAccPassword() {
-        return accPassword;
-    }
-
-    public void setAccPassword(String accPassword) {
-        this.accPassword = accPassword;
-    }
-
-    public void depositeAmount(int amount) {
-        this.accBalance += amount;
-    }
-
-    @Override
-    public String toString() {
-        return "bankModel [accNumber=" + accNumber + ", accBalance" + accBalance + ",accHolder=" + accHolder + ", accPassword=" + accPassword + "]";
-    }
 }
 
 
